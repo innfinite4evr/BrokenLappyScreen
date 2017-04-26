c2e338015c2a0eb4d1c4056c6149bc20883beedf
innfinite4evr <innfinite4evr@gmail.com>
2017-04-26 11:31:04 +0000

    setup.sh
    this is setup.sh -A shell script to tirn off broken laptop screen and display output through external LCD

---
+++ Setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo              _ _,---._ 
+echo           ,-','       `-.___ 
+echo          /-;'               `._ 
+echo         /\/          ._   _,'o \ 
+echo        ( /\       _,--'\,','"`. ) 
+echo         |\      ,'o     \'    //\ 
+echo         |      \        /   ,--'""`-. 
+echo         :       \_    _/ ,-'         `-._ 
+echo          \        `--'  /                ) 
+echo           `.  \`._    ,'     ________,',' 
+echo             .--`     ,'  ,--` __\___,;' 
+echo              \`.,-- ,' ,`_)--'  /`.,' 
+echo               \( ;  | | )      (`-/ 
+echo                 `--'| |)       |-/ 
+echo                   | | |        | | 
+echo                   | | |,.,-.   | |_ 
+echo                   | `./ /   )---`  ) 
+echo                  _|  /    ,',   ,-' 
+echo                 ,'|_(    /-<._,' |--, 
+echo                 |    `--'---.     \/ \ 
+echo                 |          / \    /\  \ 
+echo               ,-^---._     |  \  /  \  \ 
+echo            ,-'        \----'   \/    \--`. 
+echo           /            \              \   \ 
+echi Flying fekk by innfinite4evr
+xrandr --auto --output VGA-1 --mode 1440x900
+xrandr --auto --output LVDS-1 --off
